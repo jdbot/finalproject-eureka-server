80bbaa1cbaf65bbc8c8e591d86099e76251e02bb
Jorge David Prado Coloma <jpradoco@emeal.nttdata.com>
2022-12-07 22:07:43 -0500

    Subiendo eureka-server

---
+++ src/main/java/com/nttdata/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.nttdata.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Main class.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
